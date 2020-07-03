4cee9574555f322ac10c49141802a046631549e8
하영 이 <gkdudp9@naver.com>
2020-07-03 14:27:46 +0900

    HelloApp 완료

--- StudyCSarp/HelloApp/Program.cs
+++ StudyCSarp/HelloApp/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;  //Console.WriteLine에서 콘솔 생략가능
 
 namespace HelloApp
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                WriteLine("ex: HelloApp.exe<이름>");
+                return;
+            }
+            WriteLine($"Hello, {args[0]}!");
         }
     }
 }
