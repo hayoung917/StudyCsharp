9042b634d1896c9af859bc6611d5f3abc00e2ff9
하영 이 <gkdudp9@naver.com>
2020-07-06 09:50:17 +0900

    2020_07_06 Study

---
+++ StudyCSarp/EnumApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES = 10,   //값 지정 가능
+            NO = 20,
+            CANCEL = 30,
+            CONFIRM = 40,
+            OK = 50
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DialogResult.OK);
+            //Console.WriteLine((int)DialogResult.OK);
+
+            DialogResult result = DialogResult.YES;
+            if(result == DialogResult.YES)      //열거형 비교하는 방법
+            {
+                Console.WriteLine("YES를 선택했습니다.");
+            }
+        }
+    }
+}
